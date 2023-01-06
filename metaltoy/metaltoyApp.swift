f0148eb2585054da99f2e87245cef5d8e46a6e2f
Edison Moreland <edison@devdude.net>
2023-01-05 16:29:21 -0800

    Initial Commit

---
+++ metaltoy/metaltoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  metaltoyApp.swift
+//  metaltoy
+//
+//  Created by Edison Moreland on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct metaltoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
